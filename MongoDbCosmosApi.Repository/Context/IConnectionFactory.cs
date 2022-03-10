993b35f173b4ae52e7180800338a8933e4f629af
rodrigo.freitas <rodrigo.freitas@operacao.rcadigital.com.br>
2022-03-10 14:07:21 -0300

    adicionando mongo ao projeto

---
+++ MongoDbCosmosApi.Repository/Context/IConnectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDbCosmosApi.Repository.Context
+{
+    public interface IConnectionFactory
+    {
+        IMongoClient GetClient();
+        IMongoDatabase GetDatabase(IMongoClient mongoClient, string databaseName);
+        IMongoDatabase GetDatabase(string databaseName);
+    }
+}
